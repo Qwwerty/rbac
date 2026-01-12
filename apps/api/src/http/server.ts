c14b53aef764b11b668a0c9e1222d6b8afa29cfa
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2026-01-12 09:26:02 -0300

    feat: transfer organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getMemberShip } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify({
@@ -79,6 +80,7 @@ app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
+app.register(transferOrganization)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
