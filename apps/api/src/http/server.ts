d2f78f032544a150800c71ad50b95b7bac16d162
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2026-01-13 12:24:54 -0300

    feat: get project details

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,7 @@ import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/project/create-project'
 import { deleteProject } from './routes/project/delete-project'
+import { getProject } from './routes/project/get-project'
 
 const app = fastify({
   logger: {
@@ -86,6 +87,7 @@ app.register(transferOrganization)
 
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
