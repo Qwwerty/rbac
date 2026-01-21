e1c7a536d108dfdcfb1c0dc9919c86bfe2f3ecd2
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2026-01-21 16:52:13 -0300

    feat: users permissions

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,11 +2,14 @@ import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export async function Header() {
+  const permissions = await ability()
+
   return (
     <div className="mx-auto flex max-w-300 items-center justify-between">
       <div className="flex items-center gap-3">
@@ -17,6 +20,7 @@ export async function Header() {
         />
         <Slash className="text-border size-4 -rotate-24" />
         <OrganizationSwitcher />
+        {permissions?.can('get', 'Project') && <p>Projetos</p>}
       </div>
 
       <div className="flex items-center gap-4">
---
+++ apps/web/src/http/get-membership.ts
@@ -0,0 +1,18 @@
+import { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetMembershipResponse {
+  membership: {
+    id: string
+    role: Role
+    userId: string
+    organizationId: string
+  }
+}
+
+export async function getMembership(org: string) {
+  return await api
+    .get(`organizations/${org}/membership`)
+    .json<GetMembershipResponse>()
+}
